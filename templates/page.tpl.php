f141691818a91ebcdaca7f464d47560c3d845e74
will <will@xenomedia.com>
2013-08-21 16:40:45 -0500

    adjusted header portion of page template

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -8,33 +8,45 @@
               <span class="icon-bar"></span>
             </a>
 
-            <?php if (!empty($logo)): ?>
-              <a class="logo pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-                <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-              </a>
-            <?php endif; ?>
+            <div class="row-fluid">
+              <div class="span3">
+                <?php if (!empty($logo)): ?>
+                  <a class="logo pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+                    <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+                  </a>
+                <?php endif; ?>
 
-            <?php if (!empty($site_name)): ?>
-              <h1 id="site-name">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="brand"><?php print $site_name; ?></a>
-              </h1>
-            <?php endif; ?>
+                <?php if (!empty($site_name)): ?>
+                  <h1 id="site-name">
+                    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="brand"><?php print $site_name; ?></a>
+                  </h1>
+                <?php endif; ?>
+              </div>
+              <?php if (!empty($page['header'])): ?>
+                <div class="span9">
+                  <?php print render($page['header']); ?>
+                </div>
+              <?php endif; ?>
+            </div>
 
             <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-              <div class="nav-collapse collapse">
-                <nav role="navigation">
-                  <?php if (!empty($primary_nav)): ?>
-                    <?php print render($primary_nav); ?>
-                  <?php endif; ?>
-                  <?php if (!empty($secondary_nav)): ?>
-                    <?php print render($secondary_nav); ?>
-                  <?php endif; ?>
-                  <?php if (!empty($page['navigation'])): ?>
-                    <?php print render($page['navigation']); ?>
-                  <?php endif; ?>
-                </nav>
+              <div class="row-fluid">
+                <div class="nav-collapse collapse">
+                  <nav role="navigation">
+                    <?php if (!empty($primary_nav)): ?>
+                      <?php print render($primary_nav); ?>
+                    <?php endif; ?>
+                    <?php if (!empty($secondary_nav)): ?>
+                      <?php print render($secondary_nav); ?>
+                    <?php endif; ?>
+                    <?php if (!empty($page['navigation'])): ?>
+                      <?php print render($page['navigation']); ?>
+                    <?php endif; ?>
+                  </nav>
+                </div>
               </div>
-            <?php endif; ?>
+              <?php endif; ?>
+
           </div>
         </div>
       </header>
