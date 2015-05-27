698009d679c2e169cce10d75f0e09e91be640397
Jim Birch <jim@xenomedia.com>
2015-05-27 12:57:51 -0500

    Updated Block header and CSS

--- templates/block.tpl.php
+++ templates/block.tpl.php
@@ -1,4 +1,4 @@
 <div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="block block-<?php print $block->module ?>">
-    <?php if ($block->subject): ?><h2><?php print $block->subject ?></h2><?php endif;?>
+    <?php if ($block->subject): ?><div class="h2"><?php print $block->subject ?></div><?php endif;?>
     <div class="content"><?php print $block->content ?></div>
 </div>
